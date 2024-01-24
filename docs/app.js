f244f4ee1f5d2cd06e0df7649115fffe7c172ddb
sabrina01031997 <sabrinamostofasetu@gmail.com>
2024-01-24 10:21:58 -0800

    Fix error in app JS file for gst rate

--- docs/app.js
+++ docs/app.js
@@ -1,48 +1,38 @@
 'use strict';
 
 function doInvoice() {
-  // 1. Prompt for the guest's name.
-  var guestName = prompt('Adam Smith');
+  const guestName = prompt("Enter guest's name:");
 
-  // 2. Show the guest's name in the sub-heading of the page.
   document.getElementById('guestName').textContent = guestName;
 
-  // 3. Prompt for the number of nights.
-  var numberOfNights = parseInt(prompt('7'));
+  const numberOfNights = parseInt(prompt('Enter the number of nights:'), 10);
 
-  // 4. Show the number of nights in the invoice.
-  document.getElementById('numberOfNights').textContent =
-    numberOfNights + ' nights @';
+  document.getElementById('numberOfNights').textContent = numberOfNights;
 
-  // 5. Show the nightly rate using the getNightlyRate() function.
-  var nightlyRate = getNightlyRate();
-  document.getElementById('nightlyRate').textContent =
-    '$' + nightlyRate.toFixed(2) + '/night';
+  const nightlyRate = getNightlyRate();
+  document.getElementById('nightlyRate').textContent = `$${nightlyRate.toFixed(
+    2
+  )}`;
 
-  // 6. Calculate the subtotal.
-  var subtotal = nightlyRate * numberOfNights;
-  document.getElementById('subTotal').textContent = '$' + subtotal.toFixed(2);
+  const subTotal = numberOfNights * nightlyRate;
+  document.getElementById('subTotal').textContent = `$${subTotal.toFixed(2)}`;
 
-  // 7. Show the PST rate using the getTaxRate() function.
-  var pstRate = getTaxRate('PST');
-  document.getElementById('pstRate').textContent = pstRate * 100 + '%';
+  const pstRate = getTaxRate('PST');
+  document.getElementById('pstRate').textContent = `${(pstRate * 100).toFixed(
+    2
+  )}%`;
 
-  // 8. Calculate the dollar amount of the PST.
-  var pstAmount = subtotal * pstRate;
-  document.getElementById('pstAmount').textContent = '$' + pstAmount.toFixed(2);
+  const pstAmount = subTotal * pstRate;
+  document.getElementById('pstAmount').textContent = `$${pstAmount.toFixed(2)}`;
 
-  // 9. Show the GST rate using the getTaxRate() function.
-  var gstRate = getTaxRate('GST');
-  document.getElementById('gstRate').textContent = gstRate * 100 + '%';
+  const gstRate = getTaxRate('GST');
+  document.getElementById('gstRate').textContent = `${(gstRate * 100).toFixed(
+    2
+  )}%`;
 
-  // 10. Calculate the dollar amount of the GST.
-  var gstAmount = subtotal * gstRate;
-  document.getElementById('gstAmount').textContent = '$' + gstAmount.toFixed(2);
+  const gstAmount = subTotal * gstRate;
+  document.getElementById('gstAmount').textContent = `$${gstAmount.toFixed(2)}`;
 
-  // 11. Calculate the grand total.
-  var grandTotal = subtotal + pstAmount + gstAmount;
-  document.getElementById('total').textContent = '$' + grandTotal.toFixed(2);
+  const total = subTotal + pstAmount + gstAmount;
+  document.getElementById('total').textContent = `$${total.toFixed(2)}`;
 }
-
-// Add an event listener to the "New Invoice" button.
-document.getElementById('newInvoice').addEventListener('click', doInvoice);
